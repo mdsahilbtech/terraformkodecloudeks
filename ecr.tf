faa39543990aff5f6cd9bf7ef96dd7acb001db48
sahil <mdsahilbtech@gmail.com>
2024-10-25 10:05:34 +0530

    latestecr

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,10 @@
 resource "aws_ecr_repository" "frontend" {
   name                 = "frontend"
   image_tag_mutability = "MUTABLE"
-  encryption_configuration = "AES-256"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -9,7 +12,9 @@ resource "aws_ecr_repository" "frontend" {
 resource "aws_ecr_repository" "backend" {
   name                 = "backend"
   image_tag_mutability = "MUTABLE"
-  encryption_configuration = "AES-256"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
   image_scanning_configuration {
     scan_on_push = true
   }
